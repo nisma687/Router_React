8f10eec8679086f42b3234bd7254e8fec8cdceb0
Nisma <20-41982-1@student.aiub.edu>
2023-09-22 18:59:40 +0600

    router concepts-create router,link etc

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,6 @@
+
 import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 
@@ -6,11 +8,8 @@ const Home = () => {
     return (
         <div>
             <Header></Header>
-            <h2>This is Home Component</h2>
-        
-            <Outlet>
-
-            </Outlet>
+            <Outlet></Outlet>
+            <Footer></Footer>
         
         
         </div>
---
+++ src/Components/Users/Users.jsx
@@ -0,0 +1,13 @@
+
+
+const Users = () => {
+    return (
+        <div>
+            <h2>Users</h2>
+            <p>Here is a list of users</p>
+            
+        </div>
+    );
+};
+
+export default Users;
